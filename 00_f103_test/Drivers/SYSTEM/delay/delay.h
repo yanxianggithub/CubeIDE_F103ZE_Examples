1386e421166bb7a3648d038bc8045aa0efb4bfc2
yanxcg <1023305862@qq.com>
2024-11-23 16:00:34 +0800

    test

---
+++ 00_f103_test/Drivers/SYSTEM/delay/delay.h
@@ -0,0 +1,46 @@
+
+ 
+#ifndef __DELAY_H
+#define __DELAY_H
+
+#include "../sys/sys.h"
+
+
+void delay_init(uint16_t sysclk);       /* ��ʼ���ӳٺ��� */
+void delay_ms(uint16_t nms);            /* ��ʱnms */
+void delay_us(uint32_t nus);            /* ��ʱnus */
+
+#if (!SYS_SUPPORT_OS)                   /* �����֧��OS */
+    void HAL_Delay(uint32_t Delay);     /* HAL�����ʱ������HAL���ڲ��õ� */
+#endif
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 00_f103_test/Drivers/SYSTEM/sys/sys.h
@@ -0,0 +1,42 @@
+
+
+#ifndef __SYS_H
+#define __SYS_H
+
+#include "stm32f1xx.h"
+#include "main.h"
+
+/**
+ * SYS_SUPPORT_OS���ڶ���ϵͳ�ļ����Ƿ�֧��OS
+ * 0,��֧��OS
+ * 1,֧��OS
+ */
+#define SYS_SUPPORT_OS          0
+
+
+/*��������*******************************************************************************************/
+
+void sys_nvic_set_vector_table(uint32_t baseaddr, uint32_t offset);             /* �����ж�ƫ���� */
+void sys_standby(void);                                                         /* �������ģʽ */
+void sys_soft_reset(void);                                                      /* ϵͳ��λ */
+uint8_t sys_clock_set(uint32_t plln);                                           /* ʱ�����ú��� */
+void sys_stm32_clock_init(uint32_t plln);                                       /* ϵͳʱ�ӳ�ʼ������ */
+
+/* ����Ϊ��ຯ�� */
+void sys_wfi_set(void);                                                         /* ִ��WFIָ�� */
+void sys_intx_disable(void);                                                    /* �ر������ж� */
+void sys_intx_enable(void);                                                     /* ���������ж� */
+void sys_msr_msp(uint32_t addr);                                                /* ����ջ����ַ */
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
